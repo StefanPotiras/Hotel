44d7c387a3187709a6b99e46056d6c2f90bf30c7
Madalin Vladoiu <madalin.22.v@gmail.com>
2022-05-24 08:30:50 +0300

    more database methods

--- Server/Entities/Reservation.cs
+++ Server/Entities/Reservation.cs
@@ -4,15 +4,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ModelsClasses;
 namespace Server.Entities
 {
     public class Reservation
     {
-        public enum State
-        {
-            None, Active, Canceled, Paid
-        }
+       
 
         public bool Deleted { get; set; }
         public int Id { get; set; }
@@ -23,7 +20,7 @@ namespace Server.Entities
         [Column(TypeName = "datetime2")]
         public DateTime EndDate { get; set; }
 
-        public State ReservationState { get; set; }
+        public ReservationState State { get; set; }
 
         public Customer Customer { get; set; }
         public ICollection<ExtraService> ExtraServices { get; set; }
