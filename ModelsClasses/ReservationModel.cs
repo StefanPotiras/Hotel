e2639ff4225958d877b1072e945009f28d658aa7
Madalin Vladoiu <madalin.22.v@gmail.com>
2022-05-24 10:43:12 +0300

    AddReservation

--- ModelsClasses/ReservationModel.cs
+++ ModelsClasses/ReservationModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,18 +10,21 @@ using System.Threading.Tasks;
 //{ Username,Price,StartDate,EndDate,Status,RoomsNumber,RoomsType,Extra}
 namespace ModelsClasses
 {
-     class ReservationModel
+    public class ReservationModel
     {
-        
 
+        public int UserId { get; set; }
         public string Username { get; set; }
         public decimal Price { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public ReservationState State { get; set; }
         public int NumberOfRooms { get; set; }
-        public ObservableColection<ServicesModel> Services { get; set; }
-        public ObservableColection<RoomTypeNumberModel> AllRoomsWithType { get; set; }
-       
+        public ObservableCollection<ServicesModel> Services { get; set; }
+        public ObservableCollection<RoomTypeNumberModel> AllRoomsWithType { get; set; }
+
+
+
+
     }
 }
